e1f79f7f93868c7dcad5413c3a353727aa79733d
priyanshutripathi18 <priyanshutripathi532@gmail.com>
2026-09-13 23:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const nav=document.querySelector(".navbar");const menu=document.querySelector(".menu-btn");menu.addEventListener("click",()=>nav.classList.toggle("open"));document.querySelectorAll("nav a").forEach(a=>a.addEventListener("click",()=>nav.classList.remove("open")));
